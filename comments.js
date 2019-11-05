a7b050bdafe3fd1f45163b4eef4c183268e2bb57
Tubino <bruno.tubino@geneos.com.ar>
2019-11-05 18:47:20 -0300

    comment animation

---
+++ comments.js
@@ -0,0 +1,27 @@
+/* document.getElementById("dislike").addEventListener("click", animarDislike);
+document.getElementById("like").addEventListener("click", animarLike); */
+
+let elem = document.getElementsByClassName("elem")[0];
+let elem1 = document.getElementsByClassName("elem")[1];
+
+elem.classList.add("animarElem");
+elem1.classList.add("animarElem");
+
+
+/* function animarDislike(){
+
+	if(dislike.classList.contains("animarDislike")){ 
+    dislike.classList.remove("animarDislike");
+	}else{
+		dislike.classList.add("animarDislike");
+	}
+}
+function animarLike(){
+
+	if(like.classList.contains("animarLike")){ 
+        like.classList.remove("animarLike");
+	}else{
+		like.classList.add("animarLike");
+	}
+}
+ */
