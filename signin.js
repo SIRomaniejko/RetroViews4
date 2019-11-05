9769c6bb8138a332f6cde2cb0c371f25ad4333aa
Tubino <bruno.tubino@geneos.com.ar>
2019-11-05 00:06:41 -0300

    signin and login

--- signin.js
+++ signin.js
@@ -1,10 +1,16 @@
 document.getElementById('myBtnSignin').addEventListener('click', signin);
-// let user = 'usuario@gmail.com';
-// let pass = 123456;
-
+let user = 'usuario@gmail.com';
+let pass = 123456;
+document.getElementById('alert').style = "display : none";
 function signin(){
-	document.getElementById("email").style = "background-color: red";
-    // if((user==document.getElementById('email').value) &&(pass==document.getElementById('password').value)){
-        // alert("Usuario existente");
-    // }
+     if((user==document.getElementById('email').value)){
+         document.getElementById('email').style = "border: 2px solid red";
+         document.getElementById('alert').style = "display : block;margin-left:3%;color:red;";
+     }
+     else{
+        document.getElementById('email').style = "border: 1px solid green";
+        document.getElementById('error').style = "display : none;"; 
+    }
 }
+
+ 
