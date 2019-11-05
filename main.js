32530028fb73a4f90bab0d0fa9466a04d91e28b2
RomaCHEIMS <siromaniejko@gmail.com>
2019-11-05 02:36:17 -0300

    agregado buscador en celular

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ document.querySelector("#cerrarSidebar").addEventListener("click", ()=>{
     sidebar.classList.remove("abierto");
     sidebar.classList.add("cerrado");
 })
+
+document.querySelector(".lupa").addEventListener("click", ()=>{
+    document.querySelector(".nav-mobil").classList.add("hidden");
+    document.querySelector(".busquedaMobil").classList.remove("hidden");
+})
+
+document.querySelector("#busquedaReturn").addEventListener("click", ()=>{
+    document.querySelector(".nav-mobil").classList.remove("hidden");
+    document.querySelector(".busquedaMobil").classList.add("hidden");
+})
