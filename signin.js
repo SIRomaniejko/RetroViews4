3013d5bc140077a3c68e2b60b7013c7380a630d7
kappaggaeru <oliveralautaro99@gmail.com>
2019-11-04 21:53:12 -0300

    arreglado background input

--- signin.js
+++ signin.js
@@ -1,9 +1,10 @@
-let user = 'usuario@gmail.com';
-let pass = 123456;
+document.getElementById('myBtnSignin').addEventListener('click', signin);
+// let user = 'usuario@gmail.com';
+// let pass = 123456;
 
 function signin(){
-    if((user==document.getElementById('email').value) &&(pass==document.getElementById('password').value)){
-        alert("Usuario existente");
-    }
+	document.getElementById("email").style = "background-color: red";
+    // if((user==document.getElementById('email').value) &&(pass==document.getElementById('password').value)){
+        // alert("Usuario existente");
+    // }
 }
-document.getElementById('myBtnSignin').addEventListener('click', signin);
