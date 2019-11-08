637ec58db762b1b80663cf0547331a979c0f5766
SIRomaniejko <siromaniejko@gmail.com>
2019-11-08 13:46:46 -0300

    arreglado el bookmark

--- player.js
+++ player.js
@@ -1,27 +1,4 @@
- document.getElementById('secEpis').addEventListener('click', mostrarEpisodios);
- document.getElementById('secSim').addEventListener('click', mostrarSimilares);
- document.getElementById('secCom').addEventListener('click', mostrarComentarios);
 
- document.getElementById('comments').style = "display:block";
- document.getElementById('episodios').style = "display:none";
- document.getElementById('similares').style = "display:none";
-
-function mostrarEpisodios(){
-    document.getElementById('episodios').style = "display:block";
-    document.getElementById('comments').style = "display:none";
-    document.getElementById('similares').style = "display:none";
-}
-function mostrarSimilares(){
-    document.getElementById('similares').style = "display:block";
-    document.getElementById('comments').style = "display:none";
-    document.getElementById('episodios').style = "display:none";
-}
-function mostrarComentarios(){
-    document.getElementById('comments').style = "display:block";
-    document.getElementById('similares').style = "display:none";
-    document.getElementById('episodios').style = "display:none";
-
-}
 
 let avion = document.getElementById("avion");
 
@@ -35,9 +12,10 @@ document.getElementById("imgdislike").addEventListener("click",mostrarPulgares);
 let btnMiLista = document.getElementById("btnAgregarMiLista");
 btnMiLista.addEventListener("click",mostrarMiLista);
 function mostrarMiLista(){
-    document.getElementById("agregadoMiLista").classList.toggle("hidden");
+    document.getElementById("agregadoMiLista").classList.remove("hidden");
+    document.getElementById("agregadoMiLista").classList.toggle("descenso");
     setTimeout(ocultarMensaje,2000);
 }
 function ocultarMensaje(){
-    document.getElementById("agregadoMiLista").classList.toggle("hidden");
+    document.getElementById("agregadoMiLista").classList.add("hidden");
 }
