028e63ae9b04b04daea9ce1f9f513a0430883f59
kappaggaeru <oliveralautaro99@gmail.com>
2019-11-07 22:49:15 -0300

    nuevo reproductor

--- comments.js
+++ comments.js
@@ -1,41 +1,5 @@
-/* document.getElementById("dislike").addEventListener("click", animarDislike);
-document.getElementById("like").addEventListener("click", animarLike); */
-
-let elem = document.getElementsByClassName("elem")[0];
-let elem1 = document.getElementsByClassName("elem")[1];
-let elem2 = document.getElementsByClassName("elem")[2];
-let elem3 = document.getElementsByClassName("elem")[3];
-let elem4 = document.getElementsByClassName("elem")[4];
-let elem5 = document.getElementsByClassName("elem")[5];
-let elem6 = document.getElementsByClassName("elem")[6];
-let elem7 = document.getElementsByClassName("elem")[7];
-let elem8 = document.getElementsByClassName("elem")[8];
-
-elem.classList.add("animarElem");
-elem1.classList.add("animarElem");
-elem2.classList.add("animarElem");
-elem3.classList.add("animarElem");
-elem4.classList.add("animarElem");
-elem5.classList.add("animarElem");
-elem6.classList.add("animarElem");
-elem7.classList.add("animarElem");
-elem8.classList.add("animarElem");
-
-
-/* function animarDislike(){
-
-	if(dislike.classList.contains("animarDislike")){ 
-    dislike.classList.remove("animarDislike");
-	}else{
-		dislike.classList.add("animarDislike");
-	}
+let comentarios = document.getElementsByClassName("elem");
+for (let i = 0; i < comentarios.length; i++) {
+	let elem = comentarios[i];
+	elem.classList.add("animarElem");
 }
-function animarLike(){
-
-	if(like.classList.contains("animarLike")){ 
-        like.classList.remove("animarLike");
-	}else{
-		like.classList.add("animarLike");
-	}
-}
- */
